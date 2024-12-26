2b49682da23c38f45eff2e536547a97b56536a75
Arseny Sokolov <insmurfi.developer@gmail.com>
2024-12-26 23:26:44 +0300

    initial

---
+++ test.blade.php
@@ -0,0 +1,24 @@
+<html></html>
+<script>
+
+</script>
+@if()
+
+@endif
+
+<style>
+body{
+background: red;
+
+}
+</style>
+
+@dd()
+
+@for ($i = 0; $i < $count; $i++)
+    
+@endfor
+
+@extends('name')
+
+@lang('messages.welcome')
